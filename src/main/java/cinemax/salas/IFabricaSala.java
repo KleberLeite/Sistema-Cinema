79948560f91a5eb4c614e08cce3f3c2678843c6e
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-04-05 18:58:34 -0300

    doc: IFabricaSala

--- src/main/java/cinemax/salas/IFabricaSala.java
+++ src/main/java/cinemax/salas/IFabricaSala.java
@@ -1,5 +1,13 @@
 package cinemax.salas;
 
+/*
+Resumo: interface para fábricas de IEstruturaSala.
+*/
 public interface IFabricaSala {
+	
+	/*
+	Resumo: cria uma nova IEstruturaSala.
+	Return: retorna uma nova IEstruturaSala. 
+	*/
 	public IEstruturaSala criar();
 }
