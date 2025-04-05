145ae15cf34644ea71a7ad916a7b07b5eae36aad
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-04-05 18:45:05 -0300

    doc: IFabricaSessao

--- src/main/java/cinemax/sessoes/IFabricaSessao.java
+++ src/main/java/cinemax/sessoes/IFabricaSessao.java
@@ -4,6 +4,14 @@ import java.time.LocalDate;
 
 import cinemax.salas.IEstruturaSala;
 
+/*
+Resumo: interface para fábricas de sessões.
+*/
 public interface IFabricaSessao {
+	/*
+	Resumo: cria uma nova sessão a partir dos parâmetros.
+	Param(estrutura): estrutura da sala da sessão.
+	Param(inicio): data e hora do início da exibição do filme. 
+	*/
 	public ISessao criar(IEstruturaSala estrutura, LocalDate inicio);
 }
