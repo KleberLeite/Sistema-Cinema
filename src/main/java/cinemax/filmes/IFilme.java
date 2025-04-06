a18fa1aac1108fb4be4a286970c3652a39302524
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-04-06 11:09:30 -0300

    feat: adicionado opções parar alterar nome, duracaoEmMinutos, sinopse e classificacaoIndicativa de IFilme.

--- src/main/java/cinemax/filmes/IFilme.java
+++ src/main/java/cinemax/filmes/IFilme.java
@@ -5,9 +5,13 @@ import cinemax.sessoes.ISessao;
 public interface IFilme {
 	public ISessao obterTodasSessoes();
 	public String obterNome();
+	public boolean tentarAlterarNome(String novoNome);
 	public int obterDuracaoEmMinutos();
+	public boolean tentarAlterarDuracaoEmMinutos(int novaDuracaoEmMinutos);
 	public String obterSinopse();
+	public boolean tentarAlterarSinopse(String novaSinopse);
 	public ClassificacaoIndicativa obterClassificacaoIndicativa();
+	public boolean tentarAlterarClassificacaoIndicativa(ClassificacaoIndicativa novaClassificacaoIndicativa);
 	public boolean tentarAdicionarSessao(ISessao sessao);
 	public boolean tentarRemoverSessao(int idSessao);
 	public int obterIdFilme();
