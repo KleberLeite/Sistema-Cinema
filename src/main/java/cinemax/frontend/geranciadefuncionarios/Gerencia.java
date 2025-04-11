08d69a9b9cb9f37bfd1cb7c646937f074c77fab5
MatheusLuna37 <matheusbezerra18@gmail.com>
2025-04-11 19:34:38 -0300

    Craido funcionalidade de editar funcionario na planilhaa

--- src/main/java/cinemax/frontend/geranciadefuncionarios/Gerencia.java
+++ src/main/java/cinemax/frontend/geranciadefuncionarios/Gerencia.java
@@ -3,8 +3,7 @@ package cinemax.frontend.geranciadefuncionarios;
 
 import cinemax.frontend.model.Funcionarios;
 import cinemax.frontend.model.FuncionariosModel;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+
 
 
 
@@ -17,7 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 public class Gerencia extends javax.swing.JFrame {
  FuncionariosModel model=new FuncionariosModel();
     /**
-     * Criado interface de Gerenncia de funcionarios;
+     * Criado interface de Gerencia de funcionarios;
      * Funcionalidades:
      * 
      * Botão cadastrar;
@@ -40,7 +39,7 @@ public class Gerencia extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         PlanilhaGerenciaFuncionarios = new javax.swing.JTable();
         BotaoCadastrarFuncionarios = new javax.swing.JButton();
-        BotãoEditarFuncionarios = new javax.swing.JButton();
+        BotaoEditarFuncionarios = new javax.swing.JButton();
         BotãoRemoverFuncionarios = new javax.swing.JButton();
         TXTNomeFuncionarios = new javax.swing.JLabel();
         TXTCPFFuncionarios = new javax.swing.JLabel();
@@ -91,7 +90,7 @@ public class Gerencia extends javax.swing.JFrame {
             }
         });
 
-        BotãoEditarFuncionarios.setText("Editar");
+        BotaoEditarFuncionarios.setText("Editar");
 
         BotãoRemoverFuncionarios.setText("Remover");
 
@@ -165,7 +164,7 @@ public class Gerencia extends javax.swing.JFrame {
                         .addGap(164, 164, 164)
                         .addComponent(BotaoCadastrarFuncionarios)
                         .addGap(124, 124, 124)
-                        .addComponent(BotãoEditarFuncionarios)
+                        .addComponent(BotaoEditarFuncionarios)
                         .addGap(126, 126, 126)
                         .addComponent(BotãoRemoverFuncionarios)))
                 .addGap(0, 0, Short.MAX_VALUE))
@@ -205,7 +204,7 @@ public class Gerencia extends javax.swing.JFrame {
                         .addGap(272, 272, 272)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(BotaoCadastrarFuncionarios)
-                            .addComponent(BotãoEditarFuncionarios)
+                            .addComponent(BotaoEditarFuncionarios)
                             .addComponent(BotãoRemoverFuncionarios))))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 279, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -232,14 +231,14 @@ public class Gerencia extends javax.swing.JFrame {
         
         
         //Metodo validar campos de texto//
-                 limparTextos();
+                
                  if(validarCampos(nome,cpf,cargo,telefone,senha)){
-                   
+                    Funcionarios f =new Funcionarios(nome,cpf,cargo,telefone,senha);
+                this.model.CadastrarFuncionario(f);
+                
                  }
                  
-           //Criando funcionario//
-        Funcionarios f =new Funcionarios(nome,cpf,cargo,telefone,senha);
-                this.model.CadastrarFuncionario(f);
+      
                 
             //metodo para limpar campos de comando apos a criçaão do objeto//
                    limparTextos();
@@ -276,14 +275,9 @@ public class Gerencia extends javax.swing.JFrame {
         
         //Metodos para validar campos de Funcionario completo//
       public boolean validarCampos(String nome, String cpf, String cargo, String telefone, String senha){
-          if(nome.trim().isEmpty() || cpf.trim().isEmpty() || cargo.trim().isEmpty() || telefone.trim().isEmpty() || senha.trim().isEmpty()){
+          if(nome.trim().isEmpty() && cpf.trim().isEmpty() && cargo.trim().isEmpty() && telefone.trim().isEmpty() && senha.trim().isEmpty()){
      
-        //Criar caixa de atenção para campos não prenchidos//
-        Alert alerta = new Alert(AlertType.WARNING);
-        alerta.setTitle("Campos obrigatórios");
-        alerta.setHeaderText("Atenção!");
-        alerta.setContentText("Por favor, preencha todos os campos obrigatórios.");
-        alerta.showAndWait();
+        
          return false;
           }
           return true;
@@ -319,7 +313,7 @@ public class Gerencia extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BotaoCadastrarFuncionarios;
-    private javax.swing.JButton BotãoEditarFuncionarios;
+    private javax.swing.JButton BotaoEditarFuncionarios;
     private javax.swing.JButton BotãoRemoverFuncionarios;
     private javax.swing.JTextField CapturarTXTCPFFuncionarios;
     private javax.swing.JTextField CapturarTXTCargoFuncionarios;
