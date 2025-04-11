21b14ba895d966963207c4e0d1ee087bd7331a31
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-04-11 11:20:34 -0300

    doc: alimentos

--- src/main/java/cinemax/backend/alimentos/IBancoDeDadosAlimento.java
+++ src/main/java/cinemax/backend/alimentos/IBancoDeDadosAlimento.java
@@ -1,13 +1,39 @@
 package cinemax.backend.alimentos;
 
 public interface IBancoDeDadosAlimento {
+	// Retorna o alimento por seu código ou null caso não ache.
 	Alimento obterAlimentoPorCodigo(int codigo);
+	
+	// Retorna todos os alimentos que contém o parâmetro "nome" em
+	// seu nome.
 	Alimento[] obterAlimentoPorNome(String nome);
+	
+	// Retorna todos os alimentos cadastrados.
 	Alimento[] obterTodosAlimentos();
+	
+	// Retorna se existe algum alimento com o código.
 	boolean existeAlimentoComCodigo(int codigo);
+	
+	// Tenta adicionar um alimento novo, retornando falso se:
+	// 1. O nome conter no máximo 2 caracteres;
+	// 2. Se preco <= 0;
+	// 3. Se o código já foi cadastrado.
 	boolean tentardicionarAlimento(String nome, double preco, int codigo);
+	
+	// Tenta remover o alimento pelo código, retornando falso
+	// caso não ache o alimento.
 	boolean tentarRemoverAlimento(int codigo);
+	
+	// Tenta alterar o nome do alimento pelo seu código,
+	// retornando falso caso não ache o alimento.
 	boolean tentarAlterarNome(int codigo, String novoNome);
+	
+	// Tenta alterar o codigo do alimento pelo seu código,
+	// retornando falso caso não ache o alimento ou já exista
+	// outro alimento com o novo código.
 	boolean tentarAlterarCodigo(int codigo, int novoCodigo);
+	
+	// Tenta alterar o preco do alimento pelo seu código,
+	// retornando falso caso não ache o alimento.
 	boolean tentarAlterarPreco(int codigo, double novoPreco);
 }
