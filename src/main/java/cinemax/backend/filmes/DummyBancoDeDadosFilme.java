9a0ba05c7d599a379ad8604f96083342a15cdccb
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-04-16 13:45:11 -0300

    feat-dev: DummyBancoDeDadosFilme

---
+++ src/main/java/cinemax/backend/filmes/DummyBancoDeDadosFilme.java
@@ -0,0 +1,17 @@
+package cinemax.backend.filmes;
+
+import cinemax.backend.salas.IBancoDeDadosSala;
+
+public class DummyBancoDeDadosFilme extends BancoDeDadosFilme {
+	public DummyBancoDeDadosFilme(IBancoDeDadosSala bancoDeDadosSala) {
+		super(bancoDeDadosSala);
+
+		this.tentarAdicionarFilme("Cinderela e os 7 Anões", "Um filme da Cinderela com os 7 anões.", 120, ClassificacaoIndicativa.AL);
+		this.tentarAdicionarFilme("SpiderMan: O retorno", "Homem Aranha está de volta!", 180, ClassificacaoIndicativa.AL14);
+		this.tentarAdicionarFilme("Os 7 Monstrinhos", "Assustador!", 80, ClassificacaoIndicativa.AL);
+		this.tentarAdicionarFilme("Bob Sponja & Patrick", "Eu sou o Bob Esponja.", 150, ClassificacaoIndicativa.AL);
+		this.tentarAdicionarFilme("Velozes & Furiosos", "Vruuuum!", 170, ClassificacaoIndicativa.AL18);
+		this.tentarAdicionarFilme("O Sniper", "Apenas um disparo.", 130, ClassificacaoIndicativa.AL16);
+		this.tentarAdicionarFilme("MekaAnimais", "Animais mecânicos!", 50, ClassificacaoIndicativa.AL);
+	}
+}
