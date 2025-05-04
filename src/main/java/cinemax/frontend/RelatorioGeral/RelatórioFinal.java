1042d82b347b641de9657610a870a87735b2f3ac
CauaAlves347 <cicerocaua347@gmail.com>
2025-05-04 19:10:28 -0300

    feat: criação parcial focada na estilização da TelaRelatórioFinal

---
+++ src/main/java/cinemax/frontend/RelatorioGeral/RelatórioFinal.java
@@ -0,0 +1,83 @@
+package cinemax.frontend.RelatorioGeral;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import cinemax.frontend.PaginasGeranteeFuncionario.Gerente;
+
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class RelatórioFinal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RelatórioFinal frame = new RelatórioFinal();
+					frame.setVisible(true);
+					frame.setLocationRelativeTo(null);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RelatórioFinal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 64, 128));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 864, 499);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblLinha = new JLabel("__________________________________________________________________________________________________________________________");
+		lblLinha.setBounds(5, 412, 864, 14);
+		panel.add(lblLinha);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Gerente telaGerente = new Gerente();
+				telaGerente.setVisible(true);
+				telaGerente.setLocationRelativeTo(null);
+				
+				dispose();
+				
+			}
+		});
+		btnVoltar.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnVoltar.setBounds(10, 527, 89, 23);
+		contentPane.add(btnVoltar);
+		
+		JButton btnFecharSistema = new JButton("Fechar Sistema");
+		btnFecharSistema.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnFecharSistema.setBounds(720, 528, 154, 23);
+		contentPane.add(btnFecharSistema);
+	}
+}
