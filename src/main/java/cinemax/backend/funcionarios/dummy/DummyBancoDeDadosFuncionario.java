999c47ea7edd8670307f8f8a3d0004a1c3333e05
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-05-15 18:36:14 -0300

    fix: dummy funcionários gerava funcionários inválidos

--- src/main/java/cinemax/backend/funcionarios/dummy/DummyBancoDeDadosFuncionario.java
+++ src/main/java/cinemax/backend/funcionarios/dummy/DummyBancoDeDadosFuncionario.java
@@ -7,11 +7,11 @@ import cinemax.backend.funcionarios.CargoFuncionario;
 public class DummyBancoDeDadosFuncionario extends BancoDeDadosFuncionario {
 	public DummyBancoDeDadosFuncionario(Backend backend) {
 		super(backend);
-		this.internoAdicionarFuncionario("Pedro de Assis", "12345678912", CargoFuncionario.Gerente, "88988224456", "@1234");
-		this.internoAdicionarFuncionario("Paulo Fernande", "33345678912", CargoFuncionario.Gerente, "88988224455", "@5138");
-		this.internoAdicionarFuncionario("Maria Paula", "44445678912", CargoFuncionario.Atendente, "88988224454", "1$#23213");
-		this.internoAdicionarFuncionario("Mario Henrique", "55555678912", CargoFuncionario.Atendente, "88988224453", "H@#sda4");
-		this.internoAdicionarFuncionario("Joelma Alencar", "66666678912", CargoFuncionario.Atendente, "88988224452", "@12#24");
-		this.internoAdicionarFuncionario("Santos Dummond", "21987654321", CargoFuncionario.Atendente, "88988224451", "@111");
+		this.internoAdicionarFuncionario("Pedro de Assis", "12345678912", CargoFuncionario.Gerente, "88988224456", "123456");
+		this.internoAdicionarFuncionario("Paulo Fernande", "33345678912", CargoFuncionario.Gerente, "88988224455", "123456123");
+		this.internoAdicionarFuncionario("Maria Paula", "44445678912", CargoFuncionario.Atendente, "88988224454", "732161");
+		this.internoAdicionarFuncionario("Mario Henrique", "55555678912", CargoFuncionario.Atendente, "88988224453", "45612123");
+		this.internoAdicionarFuncionario("Joelma Alencar", "66666678912", CargoFuncionario.Atendente, "88988224452", "5612334");
+		this.internoAdicionarFuncionario("Santos Dummond", "10987654321", CargoFuncionario.Atendente, "88988224451", "654321");
 	}
 }
