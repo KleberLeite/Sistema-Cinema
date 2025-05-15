f63e8b36128c3497dbe7f429f44c2e79dea97195
Kleber Leite <nkltf.acc.dev@gmail.com>
2025-05-15 16:59:43 -0300

    fix: geração inválida de filmes

--- src/main/java/cinemax/backend/filmes/dummy/DummyBancoDeDadosFilme.java
+++ src/main/java/cinemax/backend/filmes/dummy/DummyBancoDeDadosFilme.java
@@ -56,33 +56,33 @@ public class DummyBancoDeDadosFilme extends BancoDeDadosFilme {
 		);
 		
 		// Dia 1
-		this.internoAdicionarSessao(0, 0, LocalDateTime.now().withHour(13).withMinute(0)); // Nemo: 13:00–14:40
-		this.internoAdicionarSessao(0, 1, LocalDateTime.now().withHour(14).withMinute(55)); // Paddington: 14:55–16:38
-		this.internoAdicionarSessao(0, 2, LocalDateTime.now().withHour(16).withMinute(55)); // Pantera: 16:55–19:16
+		this.internoAdicionarSessao(0, 0, LocalDateTime.now().withHour(11).withMinute(0)); // Nemo: 13:00–14:40
+		this.internoAdicionarSessao(0, 1, LocalDateTime.now().withHour(15).withMinute(55)); // Paddington: 14:55–16:38
+		this.internoAdicionarSessao(0, 2, LocalDateTime.now().withHour(20).withMinute(55)); // Pantera: 16:55–19:16
 
 		// Dia 2
-		this.internoAdicionarSessao(1, 3, LocalDateTime.now().plusDays(1).withHour(12).withMinute(0)); // Jogos: 12:00–14:22
+		this.internoAdicionarSessao(1, 3, LocalDateTime.now().plusDays(1).withHour(10).withMinute(0)); // Jogos: 12:00–14:22
 		this.internoAdicionarSessao(1, 4, LocalDateTime.now().plusDays(1).withHour(14).withMinute(37)); // Coringa: 14:37–16:39
-		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(1).withHour(16).withMinute(54)); // Lobo: 16:54–19:54
+		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(1).withHour(20).withMinute(54)); // Lobo: 16:54–19:54
 
 		// Dia 3
-		this.internoAdicionarSessao(0, 1, LocalDateTime.now().plusDays(2).withHour(13).withMinute(0)); // Paddington
-		this.internoAdicionarSessao(0, 3, LocalDateTime.now().plusDays(2).withHour(15).withMinute(0)); // Jogos
-		this.internoAdicionarSessao(0, 4, LocalDateTime.now().plusDays(2).withHour(17).withMinute(37)); // Coringa
+		this.internoAdicionarSessao(0, 1, LocalDateTime.now().plusDays(2).withHour(7).withMinute(0)); // Paddington
+		this.internoAdicionarSessao(0, 3, LocalDateTime.now().plusDays(2).withHour(11).withMinute(0)); // Jogos
+		this.internoAdicionarSessao(0, 4, LocalDateTime.now().plusDays(2).withHour(15).withMinute(37)); // Coringa
 
 		// Dia 4
-		this.internoAdicionarSessao(1, 2, LocalDateTime.now().plusDays(3).withHour(14).withMinute(0)); // Pantera
-		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(3).withHour(16).withMinute(36)); // Lobo
+		this.internoAdicionarSessao(1, 2, LocalDateTime.now().plusDays(3).withHour(9).withMinute(0)); // Pantera
+		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(3).withHour(14).withMinute(36)); // Lobo
 
 		// Dia 5
-		this.internoAdicionarSessao(0, 0, LocalDateTime.now().plusDays(4).withHour(12).withMinute(30)); // Nemo
-		this.internoAdicionarSessao(0, 2, LocalDateTime.now().plusDays(4).withHour(14).withMinute(25)); // Pantera
-		this.internoAdicionarSessao(0, 3, LocalDateTime.now().plusDays(4).withHour(16).withMinute(56)); // Jogos
+		this.internoAdicionarSessao(0, 0, LocalDateTime.now().plusDays(4).withHour(10).withMinute(30)); // Nemo
+		this.internoAdicionarSessao(0, 2, LocalDateTime.now().plusDays(4).withHour(15).withMinute(25)); // Pantera
+		this.internoAdicionarSessao(0, 3, LocalDateTime.now().plusDays(4).withHour(19).withMinute(56)); // Jogos
 
 		// Dia 6
-		this.internoAdicionarSessao(1, 1, LocalDateTime.now().plusDays(5).withHour(13).withMinute(15)); // Paddington
+		this.internoAdicionarSessao(1, 1, LocalDateTime.now().plusDays(5).withHour(10).withMinute(15)); // Paddington
 		this.internoAdicionarSessao(1, 4, LocalDateTime.now().plusDays(5).withHour(15).withMinute(33)); // Coringa
-		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(5).withHour(17).withMinute(35)); // Lobo
+		this.internoAdicionarSessao(1, 5, LocalDateTime.now().plusDays(5).withHour(20).withMinute(35)); // Lobo
 
 		// Dia 7
 		this.internoAdicionarSessao(0, 0, LocalDateTime.now().plusDays(6).withHour(11).withMinute(0)); // Nemo
